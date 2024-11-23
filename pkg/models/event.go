0ec70193c54ffd7cee5b63efdc3f25afa1c3633d
L4B0MB4 <metalfan65@gmail.com>
2024-11-23 16:42:10 +0100

    serializing aggreagteId

--- pkg/models/event.go
+++ pkg/models/event.go
@@ -5,6 +5,6 @@ type Event struct {
 	Version       int64  `json:"version" binding:"required"`
 	Name          string `json:"name" binding:"required"`
 	Data          []byte `json:"data" binding:"required"`
-	AggregateId   string `json:"-"`
+	AggregateId   string `json:"aggregateId"`
 	AggregateType string `json:"aggregateType" binding:"required"`
 }

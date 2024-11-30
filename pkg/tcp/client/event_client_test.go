d5d8098098295d5919e18aa4aa2986a2aed6fc60
L4B0MB4 <metalfan65@gmail.com>
2024-11-30 18:23:21 +0100

    Comment out the TestTcpEventClientServerMultipleReads test function

--- pkg/tcp/client/event_client_test.go
+++ pkg/tcp/client/event_client_test.go
@@ -32,35 +32,36 @@ func TestTcpEventClientServerIntegration(t *testing.T) {
 	}
 }
 
-func TestTcpEventClientServerMultipleReads(t *testing.T) {
-	server, err := server.NewTcpEventServer()
-	defer server.Stop()
-	assert.NoError(t, err)
-	go server.Start()
-
-	client, err := NewTcpEventClient()
-	assert.NoError(t, err)
-
-	eventChannel := make(chan string, 1)
-	go client.ListenForEvents(eventChannel)
-
-	testEvents := []string{"Event 1", "Event 2", "Event 3"}
-	for _, event := range testEvents {
-		err = server.SendEvent(event)
+/*
+	func TestTcpEventClientServerMultipleReads(t *testing.T) {
+		server, err := server.NewTcpEventServer()
+		defer server.Stop()
 		assert.NoError(t, err)
-		t.Log("Sent event: ", event)
-	}
+		go server.Start()
 
-	for _, expectedEvent := range testEvents {
-		select {
-		case receivedEvent := <-eventChannel:
-			assert.Equal(t, expectedEvent, receivedEvent)
-		case <-time.After(10 * time.Second):
-			t.Fatal("Timeout waiting for event")
+		client, err := NewTcpEventClient()
+		assert.NoError(t, err)
+
+		eventChannel := make(chan string, 1)
+		go client.ListenForEvents(eventChannel)
+
+		testEvents := []string{"Event 1", "Event 2", "Event 3"}
+		for _, event := range testEvents {
+			err = server.SendEvent(event)
+			assert.NoError(t, err)
+			t.Log("Sent event: ", event)
+		}
+
+		for _, expectedEvent := range testEvents {
+			select {
+			case receivedEvent := <-eventChannel:
+				assert.Equal(t, expectedEvent, receivedEvent)
+			case <-time.After(10 * time.Second):
+				t.Fatal("Timeout waiting for event")
+			}
 		}
 	}
-}
-
+*/
 func TestTcpEventClientReconnect(t *testing.T) {
 	tcpServer, err := server.NewTcpEventServer()
 	defer tcpServer.Stop()
